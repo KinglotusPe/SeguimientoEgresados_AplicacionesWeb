9a0aa6b617b0f828fc1be45e3dbdef3206e2c9e4
KinglotusPe <luzacordovacesar@gmail.com>
2026-08-20 15:42:18 -0500

    Semana07 correccion de base de datos y reestructuracion del proyecto de la semana06

---
+++ backend/proyectoinventario/src/test/java/aplicacionesweb/proyectoinventario/ProyectoinventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package aplicacionesweb.proyectoinventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoinventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
